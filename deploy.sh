7251f4d63047444191aa80042a0338bcad2bec1f
Ross McKelvie <iam@rossmckelvie.com>
2017-12-12 21:43:40 -0600

    invalidate cache after syncing files

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
+# Sync files
 aws s3 sync . s3://austinfpv.racing \
     --exclude="*.git*" \
     --exclude="*.DS_Store" \
     --exclude="deploy.sh" \
     --exclude="*.less"
+
+# Invalidate cache
+aws cloudfront create-invalidation --distribution-id E248HUNS5N052F --paths /index.html /css/*
