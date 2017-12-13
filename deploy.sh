716ad5c7c5317b075ad2555e17b187b199690dc5
Ross McKelvie <iam@rossmckelvie.com>
2017-12-12 22:11:34 -0600

    cleanup for mobile, invalidate root

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ aws s3 sync . s3://austinfpv.racing \
     --exclude="*.less"
 
 # Invalidate cache
-aws cloudfront create-invalidation --distribution-id E248HUNS5N052F --paths /index.html /css/*
+aws cloudfront create-invalidation --distribution-id E248HUNS5N052F --paths / /index.html /css/*
