60913f5903b96bb3d3df14df4b68a36b8bd1f85f
Ross McKelvie <iam@rossmckelvie.com>
2018-07-27 01:34:22 -0500

    Add RQ; improve dev/build process

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
+# Create directory for deployment
+mkdir -p public
+
 # Compile less
 lessc css/main.less public/css/main.css
 
-# Move html (nothing to compile yet)
+# Move html, img, js
 cp index.html public/index.html 
+cp -R img public
+cp -R js public
 
 # Sync files
 aws s3 sync public s3://austinfpv.racing \
@@ -13,3 +18,6 @@ aws s3 sync public s3://austinfpv.racing \
 
 # Invalidate cache
 aws cloudfront create-invalidation --distribution-id E248HUNS5N052F --paths / /index.html /css/*
+
+# Delete build directory
+rm -rf public
